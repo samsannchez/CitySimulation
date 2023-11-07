aff4542e54f5500f9f987675214671f92d879245
Sam Sanchez Juarez <69266761+samsannchez@users.noreply.github.com>
2023-11-07 14:41:59 -0500

    Update BuildingType.h

---
+++ BuildingType.h
@@ -0,0 +1,63 @@
+#ifndef _BUILDINGTYPE_H_
+#define _BUILDINGTYPE_H_
+
+
+//Max/Min Heights for each building
+#define STANDARD_MAX_HEIGHT 30
+#define STANDARD_MIN_HEIGHT 4
+
+#define STACKED_MAX_HEIGHT 100
+#define STACKED_MIN_HEIGHT 60
+
+#define STATE_MAX_HEIGHT 200
+#define STATE_MIN_HEIGHT 100
+
+#define MODERN_MAX_HEIGHT 64
+#define MODERN_MIN_HEIGHT 32
+#define SKIP_RANGE 90
+#define SKIP_PROB 85
+
+#define BLOCKS_MAX_HEIGHT 80
+#define BLOCKS_MIN_HEIGHT 50
+
+#define MIN_HELIPAD_HEIGHT 80
+
+enum BuildingType
+{
+	STANDARD,
+	STACKED,
+	STATE,
+	MODERN,
+	BLOCKS,
+	SPIRE,
+	FIB,
+	NONE
+};
+
+enum ModernFaces
+{
+	EIGHT = 8,
+	TWELVE = 12,
+	TWENTY_FOUR = 24,
+	THIRTY_SIX = 36
+};
+
+ModernFaces randomModernFaces()
+{
+	int choice = rand() % 4;
+
+	switch(choice)
+	{
+	case 0:
+		return TWELVE;
+	case 1:
+		return TWENTY_FOUR;
+	case 2:
+		return THIRTY_SIX;
+	case 3:
+		return EIGHT;
+	}
+	return EIGHT;
+}
+
+#endif
